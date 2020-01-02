8c459dbf384c520b92b4e9df54af97fc82a8c674
Vincent Tieu <vktieu@Vincents-MacBook-Pro.local>
2020-01-02 13:37:55 -0800

    list

--- test01/App.js
+++ test01/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View, TouchableHighlight,} from 'react-native';
-
+import { StyleSheet, Text, View, TouchableWithoutFeedback, FlatList} from 'react-native';
+let list = ['', 'Apple', 'Orange', 'Pears', 'Bananas', 'Rice', 'Chicken', 'Watermelon', 'Pho', 'Chow Mein', 'Beef', ''];
 const styles = StyleSheet.create({
-  bigBlue: {
+
+  titleWhite: {
     color: 'white',
     fontSize: 35,
-    fontFamily: 'Futura-MediumItalic'
+    fontFamily: 'Futura-MediumItalic',
+    marginBottom: 5, 
+    paddingLeft:10, 
+    paddingRight:10,
   },
+
+  listItemWhite: {
+    color: 'white',
+    fontSize: 25,
+    fontFamily: 'Avenir',
+    marginBottom: 3, 
+    paddingLeft:10, 
+    paddingRight:10,
+  },
+
 });
 
 export default class FlexDirectionBasics extends Component {
@@ -28,27 +42,59 @@ export default class FlexDirectionBasics extends Component {
             flex: 1, 
             flexDirection: 'row',
             justifyContent: 'space-between', 
-            alignItems: 'flex-end', 
-            marginBottom: 5, 
-            paddingLeft:10, 
-            paddingRight:10,
+            alignItems: 'flex-end',
           }}>
             
-            <TouchableHighlight style={styles.bigBlue} onPress={this._onPressButton}>
-              <Text style={styles.bigBlue}>+</Text> 
-            </TouchableHighlight>
+            <TouchableWithoutFeedback style={styles.titleWhite
+        } onPress={this._onPressButton}>
+              <Text style={styles.titleWhite
+          }>+</Text> 
+            </TouchableWithoutFeedback>
 
-            <Text style={styles.bigBlue}>refridger</Text>
+            <Text style={styles.titleWhite
+        }>refridger</Text>
 
-            <TouchableHighlight style={styles.bigBlue} onPress={this._onPressButton}>
-              <Text style={styles.bigBlue}>---</Text>
-            </TouchableHighlight>
+            <TouchableWithoutFeedback style={styles.titleWhite
+        } onPress={this._onPressButton}>
+              <Text style={styles.titleWhite
+          }>---</Text>
+            </TouchableWithoutFeedback>
           </View>
         </View>
 
         <View style={{
           flex: 1, 
-          backgroundColor: 'skyblue'}} />
+          backgroundColor: 'skyblue',
+        }}>
+          <View style={{marginLeft: 30, marginTop: 0,}}>
+            <FlatList
+              data={[
+                {key: ' '},
+                {key: 'Apples'},
+                {key: 'Oranges'},
+                {key: 'Pears'},
+                {key: 'Bananas'},
+                {key: 'Rice'},
+                {key: 'Chicken'},
+                {key: 'Watermelon'},
+                {key: 'Pho'},
+                {key: 'Chow Mein'},
+                {key: 'Beef'},
+                {key: ' '},
+                {key: ' '},
+                {key: ' '},
+                {key: ' '},
+                {key: ' '},
+                {key: ' '},
+                {key: ' '},
+                {key: ' '},
+
+              ]}
+              renderItem= {({item}) => <Text style={styles.listItemWhite
+          }>{item.key}</Text>}
+            />
+          </View>
+        </View>
       </View>
       
     );
