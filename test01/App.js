2fd79be5ba59bf36f5647324b589872ef14e67fd
Vincent Tieu <vktieu@Vincents-MacBook-Pro.local>
2020-01-02 03:03:41 -0800

    plus and minus

--- test01/App.js
+++ test01/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableHighlight,} from 'react-native';
+
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'white',
+    fontSize: 35,
+    fontFamily: 'Futura-MediumItalic'
+  },
+});
 
 export default class FlexDirectionBasics extends Component {
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+  
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
       <View style={{
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'stretch',
+        // flexDirection: 'column',
+        // justifyContent: 'flex-start',
+        // alignItems: 'stretch',
       }}>
-        <View style={{height: 100, backgroundColor: 'powderblue'}} />
+        <View style={{height: 100, backgroundColor: 'powderblue', marginBottom: 3}}>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end', marginBottom: 5, paddingLeft:10, paddingRight:10}}>
+            <TouchableHighlight style={styles.bigBlue} onPress={this._onPressButton}>
+              <Text style={styles.bigBlue}>+</Text>
+            </TouchableHighlight>
+            <Text style={styles.bigBlue}>refridger</Text>
+            <TouchableHighlight style={styles.bigBlue} onPress={this._onPressButton}>
+              <Text style={styles.bigBlue}>---</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+
         <View style={{flex: 1, backgroundColor: 'skyblue'}} />
       </View>
       
