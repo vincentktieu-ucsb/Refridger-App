d0b85ac86bcfd948b5cb91f32e967b58e5d7d30a
Vincent Tieu <vktieu@Vincents-MacBook-Pro.local>
2020-01-02 12:30:15 -0800

    cleaning

--- test01/App.js
+++ test01/App.js
@@ -18,23 +18,37 @@ export default class FlexDirectionBasics extends Component {
     return (
       <View style={{
         flex: 1,
-        // flexDirection: 'column',
-        // justifyContent: 'flex-start',
-        // alignItems: 'stretch',
       }}>
-        <View style={{height: 100, backgroundColor: 'powderblue', marginBottom: 3}}>
-          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end', marginBottom: 5, paddingLeft:10, paddingRight:10}}>
+        <View style={{
+          height: 100, 
+          backgroundColor: 'powderblue',
+          marginBottom: 3,
+        }}>
+          <View style={{
+            flex: 1, 
+            flexDirection: 'row',
+            justifyContent: 'space-between', 
+            alignItems: 'flex-end', 
+            marginBottom: 5, 
+            paddingLeft:10, 
+            paddingRight:10,
+          }}>
+            
             <TouchableHighlight style={styles.bigBlue} onPress={this._onPressButton}>
-              <Text style={styles.bigBlue}>+</Text>
+              <Text style={styles.bigBlue}>+</Text> 
             </TouchableHighlight>
+
             <Text style={styles.bigBlue}>refridger</Text>
+
             <TouchableHighlight style={styles.bigBlue} onPress={this._onPressButton}>
               <Text style={styles.bigBlue}>---</Text>
             </TouchableHighlight>
           </View>
         </View>
 
-        <View style={{flex: 1, backgroundColor: 'skyblue'}} />
+        <View style={{
+          flex: 1, 
+          backgroundColor: 'skyblue'}} />
       </View>
       
     );
